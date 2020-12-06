2ae2ef26979d6a18f16a703995b822dbc053895f
TChristensenDrumz <tchristensendrumz@gmail.com>
2020-12-06 14:33:42 -0800

    Fixed bug in /api/notes route where json would not appear

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,41 @@
-// Dependencies
 const fs = require("fs");
 const path = require("path");
 
-// ROUTING
 module.exports = function(app) {
-    app.get("/api/notes", function(req, res) {
-        const db = fs.readFile("../db/db.json", (err) => err ? console.error(err) : console.log(db));
-        res.json(db);
-    });
+    fs.readFile(path.join(__dirname, "../db/db.json"), function(err, data) {
+        if (err) throw err;
+        let notes = JSON.parse(data);
+    
+        app.get("/api/notes", function(req, res) {
+            res.json(notes);
+        });
 
-    app.post("/api/notes", function(req, res) {
-        const db = fs.readFile("../db/db.json", (err) => err ? console.error(err) : console.log(db));
-        db.push(req);
-        fs.writeFile("../db/db.json", db, (err) => err ? console.error(err) : console.log("Note added"));
-        res.json(db);
+        app.post("/api/notes", function(req, res) {
+            const newNote = req.body;
+
+            newNote.id = newNote.title.replace(/\s+/g, "").toLowerCase();
+
+            notes.push(newNote);
+
+            res.json(newNote);
+        });
+
+        app.get("/api/notes/:id", function(req, res) {
+            for(let i = 0; i < notes.length; i++) {
+                if(req.params.id === notes[i].id) {
+                    return res.json(notes[i]);
+                }
+            }
+
+            return res.json(false);
+        });
+
+        app.delete("/api/notes/:id", function(req, res) {
+            for(let i = 0; i < notes.length; i++) {
+                if(req.params.id === notes[i].id) {
+                    notes.splice(i, 1);
+                }
+            }
+        });
     });
 }
